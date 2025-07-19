7b1abc4929d79872d6437967770a1c3a5c0a23b4
Kesarikumaran <kesarikumaran@gmail.com>
2025-07-19 13:33:07 +0530

    Tried to shorten the SHA msg

--- src/app.py
+++ src/app.py
@@ -11,7 +11,7 @@ def details():
         'version': '1.0.0',
         'hostname': socket.gethostname(),
         'description': 'This is a sample Flask application for demonstration purposes.',
-        'author': 'Kesarikumaran S - CSE - A'
+        'author': 'Kesarikumaran S - CSE - A - REC'
     })
 
 @app.route('/api/v1/healthz')

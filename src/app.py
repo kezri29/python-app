05806514b07a3e4fb747e57f71e4b5d6a51f34b8
Kesarikumaran <kesarikumaran@gmail.com>
2025-07-20 13:04:43 +0530

    Used self hosted github runner

--- src/app.py
+++ src/app.py
@@ -11,7 +11,7 @@ def details():
         'version': '1.0.0',
         'hostname': socket.gethostname(),
         'description': 'This is a sample Flask application for demonstration purposes.',
-        'author': 'Kesarikumaran S - CSE - A - REC'
+        'author': 'Kesarikumaran S - CSE - A - REC - Thandalam'
     })
 
 @app.route('/api/v1/healthz')
